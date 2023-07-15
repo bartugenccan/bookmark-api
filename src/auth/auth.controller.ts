8d2e69597297b01f029970efc93a3caa8eea9349
Bartu Gençcan <bartugenccandev@gmail.com>
2023-07-15 22:08:50 +0300

    feat: Sign Up method created in Auth Service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,22 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
+
+// Service
 import { AuthService } from './auth.service';
+
+// DTO
+import { RegisterDto } from './dto';
+
+// Model
+import { User } from '@prisma/client';
 
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) { }
 
-    @Get()
-    getHello(): string {
-        return this.authService.getHello();
+    @Post('signup')
+    async signUp(@Body() registerDto: RegisterDto): Promise<User> {
+        return this.authService.signUp(registerDto);
     }
+
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,11 +3,53 @@ import { Injectable } from '@nestjs/common';
 // Services
 import { PrismaService } from 'src/prisma/prisma.service';
 
+// DTOS
+import { LoginDto, RegisterDto } from './dto';
+
+// Models
+import { User } from '@prisma/client';
+
+// Argon
+import * as argon from 'argon2';
+
 @Injectable()
 export class AuthService {
 
     constructor(private prisma: PrismaService) { }
-    getHello(): string {
-        return 'Hello World!';
+
+    /**
+     * Sign up a user.
+     *
+     * @param {RegisterDto} registerDto - The registration data for the user.
+     * @returns {Promise<User>} A promise that resolves to the created user.
+     */
+    async signUp(registerDto: RegisterDto): Promise<User> {
+        const { email, password, firstName, lastName } = registerDto;
+
+        const hash: string = await argon.hash(password);
+
+        const newUser = await this.prisma.user.create({
+            data: {
+                email,
+                hash,
+                firstName,
+                lastName,
+            },
+            select: {
+                id: true,
+                email: true,
+                createdAt: true,
+                firstName: true,
+                lastName: true
+            }
+        }).catch((error) => {
+            throw new Error(`Failed to sign up user: ${error.message}`);
+        });
+
+        return newUser as User;
+    }
+
+
+    signIn() {
     }
 }
